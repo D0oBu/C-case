b3a34bd6a07441f52024285039aeb5acc6376817
Ekaboo <3103117281@student.smktelkom-pwt.sch.id>
2021-09-11 00:19:36 +0700

    jal

---
+++ case2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	char nama[30] = "Mawar";
+	char jurusan[35] = "Teknik Informatika";
+	char univ[50] = "Universitas Dian Nuswantoro";
+	
+	cout<<"Nama saya "<<nama<<"\nJurusan: "<<jurusan<<"\nUniversitas: "<<univ<<endl;
+	return 0;
+}
